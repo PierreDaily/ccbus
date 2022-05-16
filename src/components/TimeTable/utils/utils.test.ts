aff6aeea4798e4176d9e7349e6114998d7cc77ea
Pierre <pierre@pop-os.localdomain>
2022-05-16 23:09:55 +0800

    fix: twentyFourHToIsoDateString function accept timeString starting with 01, 02, ..., 09

--- src/components/TimeTable/utils/utils.test.ts
+++ src/components/TimeTable/utils/utils.test.ts
@@ -1,6 +1,12 @@
 import { twentyFourHToIsoDateString } from "./utils";
 
 describe("twentyFourHToIsoDateString ", () => {
+
+  afterEach(() => {
+    // restoring date after each test run
+    vi.useRealTimers()
+  })
+
   it("should return an ISO dateString", () => {
     // arrange
 
@@ -25,17 +31,44 @@ describe("twentyFourHToIsoDateString ", () => {
     expect(result).toBe(expectedDate.toISOString());
   });
 
-  it("should return null is the input isn't at HH:MM format", () => {
+  it("should return an ISO dateString if the input at 'H:MM' format", () => {
     // arrange
 
-    const input = "02:22";
+    vi.useFakeTimers();
+    const now = new Date("2000-01-01T07:30:00.000");
+
+    vi.setSystemTime(now);
+
+    const expectedDate = new Date();
+    expectedDate.setHours(8);
+    expectedDate.setMinutes(2);
+    expectedDate.setSeconds(0);
+    expectedDate.setMilliseconds(0);
+
+    const timeString = "8:02";
 
     // act
-
-    const result = twentyFourHToIsoDateString(input);
+    // expect(expectedDate.toISOString()).toBe(2);
+    const result = twentyFourHToIsoDateString(timeString);
 
     // assert
 
-    expect(result).toBe(null);
+    expect(result).toBe(expectedDate.toISOString());
+  });
+
+  it("should return null if the input start with more than 2 zero", () => {
+    // arrange
+
+
+
+    const timeString = "000:02";
+
+    // act
+    // expect(expectedDate.toISOString()).toBe(2);
+    const result = twentyFourHToIsoDateString(timeString);
+
+    // assert
+
+    expect(result).toBeNull();
   });
 });
--- src/components/TimeTable/utils/utils.ts
+++ src/components/TimeTable/utils/utils.ts
@@ -1,5 +1,5 @@
 export function twentyFourHToIsoDateString(timeString: string) {
-  if (!RegExp("^(1?[0-9]|2[0-3]):[0-5][0-9]$").test(timeString)) {
+  if (!RegExp("^((0|1)?[0-9]|2[0-3]):[0-5][0-9]$").test(timeString)) {
     return null;
   }
   const newDate = new Date();

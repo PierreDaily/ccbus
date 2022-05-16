86cb5c448dc16a746888a707de781f1d04371df7
Pierre <pierre@pop-os.localdomain>
2022-05-16 13:41:08 +0800

    feat: TimeSlot component

---
+++ src/components/TimeSlot/TimeSlot.stories.js
@@ -0,0 +1,33 @@
+import TimeSlot from './index.vue';
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'TimeSlot',
+  component: TimeSlot,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    isActive: { control: 'boolean' },
+    isFocus: { control: 'boolean' },
+    time: {control: { type: 'select' },
+    options: ['12:00 am', '0:00pm', undefined],
+  },
+    timeLeft: {control: { type: 'select' },
+    options: ['3 min', '13 min', undefined],
+  },}
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { TimeSlot },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<time-slot v-bind="args" />',
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+
---
+++ src/components/TimeSlot/TimeSlot.test.ts
@@ -0,0 +1,94 @@
+import { mount } from "@vue/test-utils";
+import TimeSlot from "./index.vue";
+
+describe("TimeSlot component", () => {
+    it("should mount", async () => {
+        // arrange
+        expect(TimeSlot).toBeTruthy();
+      
+        const wrapper = mount(TimeSlot, {
+          props: {
+            time: "12:00am",
+            timeLeft: "2 min",
+            isActive: false,
+            isFocus: false,
+          },
+        });
+      
+        // assert
+        expect(wrapper.html()).toMatchSnapshot();
+      });
+
+      it("should have 'disable' class if isActive props is false", () => {
+        // arrange
+      
+        const wrapper = mount(TimeSlot, {
+          props: {
+            time: "12:00am",
+            timeLeft: "2 min",
+            isActive: false,
+            isFocus: false,
+          },
+        });
+      
+        //assert
+      
+          expect(wrapper.find("li").html()).toContain(
+        'class="disable"'
+      );
+    });
+
+    it("should have 'disable' class if isActive props is false", () => {
+        // arrange
+      
+        const wrapper = mount(TimeSlot, {
+          props: {
+            time: "12:00am",
+            timeLeft: "2 min",
+            isActive: true,
+            isFocus: false,
+          },
+        });
+      
+        //assert
+      
+          expect(wrapper.find("li").html()).not.toContain(
+        'class="disable"'
+      );
+    });
+    
+    it("should emit a 'scrollToActiveSlot' on mount if isFocus props is true", () => {
+      // arrange
+    
+      const wrapper = mount(TimeSlot, {
+        props: {
+          time: "12:00am",
+          timeLeft: "2 min",
+          isActive: false,
+          isFocus: true,
+        },
+      });
+    
+      //assert
+    
+      expect(wrapper.emitted("scrollToActiveSlot")).toHaveLength(1);
+    });
+    
+    it("shouldn't emit a 'scrollToActiveSlot' on mount if isFocus props is false", () => {
+        // arrange
+      
+        const wrapper = mount(TimeSlot, {
+          props: {
+            time: "12:00am",
+            timeLeft: "2 min",
+            isActive: false,
+            isFocus: false,
+          },
+        });
+      
+        //assert
+      
+        expect(wrapper.emitted("scrollToActiveSlot")).toBeUndefined();
+      });
+});
+

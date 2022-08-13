4561e93282f4655e4d4764aefc99d3ee4811f0fd
Pierre <pierre@pop-os.localdomain>
2022-08-13 17:43:36 +0200

    style - add extended color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,21 @@ module.exports = {
   "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'bus-green': 'rgb(39, 174, 96)',
-      'grey-light': 'rgba(222, 222, 230, 1)',
-      'black-light': 'rgba(73, 80, 87, 1)'
-    },
+    // colors: {
+    //   'bus-green': 'rgb(39, 174, 96)',
+    //   'grey-light': 'rgba(222, 222, 230, 1)',
+    //   'black-light': 'rgba(73, 80, 87, 1)'
+    // },
     fontFamily: {
       sans: ['Roboto', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        'bus-green': 'rgb(39, 174, 96)',
+        'grey-light': 'rgba(222, 222, 230, 1)',
+        'black-light': 'rgba(73, 80, 87, 1)'
+      },
+    },
   },
   plugins: [],
 }

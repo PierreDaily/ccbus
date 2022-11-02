e1212fe64ed7719cfaf90199cd5b31c4b4ac97bc
Pierre <contact@pierredaily.com>
2022-11-02 14:50:40 +0800

    feat - google analytics integration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueGtag from "vue-gtag";
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(VueGtag, {
+    config: { id: import.meta.env.PROD && import.meta.env.Vite_GOOGLE_ANALYTICS_ID },
+  })
+  .mount("#app");

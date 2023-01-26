af05034bfb391b68172fcfcf14945dc684f1811d
Pierre DAILY <contact@pierredaily.com>
2023-01-26 21:47:35 +0800

    test - unit test for minutesLeft and isPublicHoliday function

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,6 +1,6 @@
-import { isPublicHoliday } from ".";
+import { isPublicHoliday,minutesLeft } from ".";
 
-test("isPublicHoliday function", () => {
+describe("isPublicHoliday function", () => {
   it("should return true given that the input date is public holiday", () => {
     // arange
     const inputDate = new Date("2023/06/14");
@@ -18,4 +18,21 @@ test("isPublicHoliday function", () => {
 
     expect(isPublicHoliday(publicHolidayList, inputDate)).toBeFalsy();
   });
+});
+
+describe("minutesLeft function", () => {
+  it("should return 3 if the time difference is 120sec", () => {
+    const date1 = new Date("2023/06/14");
+    const date2 = new Date("2023/06/14");
+    date1.setSeconds(120)
+
+    expect(minutesLeft(date1,date2)).toBe(3)
+  });
+
+  it("should return 1 if there is no time difference", () => {
+    const date = new Date("2023/06/14");
+
+    expect(minutesLeft(date,date)).toBe(1)
+  });
+
 });

79e24bb2ac13fca4d6f17c4c11468d90e2361d2f
Pierre DAILY <contact@pierredaily.com>
2023-02-15 15:31:14 +0800

    fix - tailwind custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,12 @@ module.exports = {
       colors: {
         'bus-green': 'rgb(39, 174, 96)',
         'grey-light': 'rgba(222, 222, 230, 1)',
-        'black-light': 'rgba(73, 80, 87, 1)'
+        'black-light': 'rgba(73, 80, 87, 1)',
+        'dark-green': '#417293'
       },
+      width: {
+        '18': '4.5rem',
+      }
     },
   },
   plugins: [],

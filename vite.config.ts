85b7ec07a78030668fb214b03859ee754063d8cf
Pierre DAILY <contact@pierredaily.com>
2023-08-13 13:30:37 +0800

    build - update vite, plugins, config and typescript

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      }
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });

43835dd66fb985f5a0c9abba9248f1058e978aad
Pierre DAILY <contact@pierredaily.com>
2024-08-08 10:52:31 +0800

    REFACTO - TimeDisplay component using "setup" syntax

--- src/components/TimeDisplay/index.vue
+++ src/components/TimeDisplay/index.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="h-5 w-[90px] ">
-    <img class="inline h-2.5 w-2.5 relative left-[-2px] top-[-4px] " :src="clockIcon" />
-    <span class="text-black-light inline-block font-sans text-lg font-normal leading-5 text-right">{{time || '--:--'}}</span>
+  <div class="h-5 w-[90px]">
+    <img
+      class="inline h-2.5 w-2.5 relative left-[-2px] top-[-4px]"
+      :src="clockIcon"
+    />
+    <span
+      class="text-black-light inline-block font-sans text-lg font-normal leading-5 text-right"
+      >{{ time || "--:--" }}</span
+    >
   </div>
 </template>
 
-<script lang="ts">
-
+<script lang="ts" setup>
 import clockIcon from "@/assets/icons/clock.svg";
-export default {
-  name: "TimeDisplay",
-  props: {
-    time: String
-  },
-  setup() {
-    return {
-      clockIcon,
-    };
-  },
-};
+const { time } = defineProps({ time: String });
 </script>

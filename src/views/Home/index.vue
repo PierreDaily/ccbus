d49f398f80a749ac551f0f018d50d96abee617eb
Pierre DAILY <contact@pierredaily.com>
2025-01-29 17:52:49 +0100

    CHORE - TImeTable last update

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div class="flex justify-between items-center">
       <h1 class="items-center text-base text-white flex font-sans text-normal">
-        Timetables / updated on 14/11/2024
+        Timetables / updated on 29/01/2025
       </h1>
       <nav class="flex w-16 justify-between">
         <MailTo :email="email" />
